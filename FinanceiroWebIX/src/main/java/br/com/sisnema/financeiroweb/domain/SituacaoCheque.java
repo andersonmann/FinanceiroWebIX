584ed46cc86229521a8194ff8e3ea128d6bf7879
Anderson Mann <anderson.civil@hotmail.com>
2016-06-18 16:48:49 -0300

    adicionado classes cheque

---
+++ FinanceiroWebIX/src/main/java/br/com/sisnema/financeiroweb/domain/SituacaoCheque.java
@@ -0,0 +1,18 @@
+package br.com.sisnema.financeiroweb.domain;
+
+public enum SituacaoCheque {
+	BAIXADO("Baixado"),
+	CANCELADO("Cancelado"),
+	NAO_EMITIDO("Não emitido");
+
+	private String descricao;
+
+	private SituacaoCheque(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return descricao;
+	}
+}
